03226476bbf4d2f9557c48e1c498b5b87797bd7d
Yuvraj Dagur <yuvraj.dagur928@gmail.com>
2022-03-14 02:22:57 +0530

    modified CSS, rebuilt color palette, removed caching for local development

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const url = "https://www.boredapi.com/api/activity";
-
+var darkTheme = true;
 async function getActivity() {
     fetch(url).then(function(response) {
         return response.json();
@@ -11,5 +11,21 @@ async function getActivity() {
         console.log(e);
     });
 };
+
+function changeTheme() {
+    darkTheme = !darkTheme;
+    const root = document.querySelector(":root");
+    if (darkTheme) {
+        console.log(darkTheme);
+        root.style.setProperty("--background-theme", "#040d19");
+        root.style.setProperty("--background-theme-text", "#d2e3f7");
+        root.style.setProperty("--background-theme-complementary", "#1C3144");
+    } else {
+        console.log(darkTheme);
+        root.style.setProperty("--background-theme", "#f2f2f2");
+        root.style.setProperty("--background-theme-text", "#060e24");
+        root.style.setProperty("--background-theme-complementary", "#d9e0ed");
+    }
+}
 
 document.addEventListener("DOMContentLoaded", getActivity);
--- sw.js
+++ sw.js
@@ -4,22 +4,18 @@ const cacheName = 'unBore.me';
 self.addEventListener('install', e => {
     e.waitUntil(
         caches.open(cacheName).then(cache => {
-            // Our application only has two files here index.html and manifest.json
-            // but you can add more such as style.css as your app grows
-            return cache.addAll([
-                './',
-                './index.html',
-                './manifest.json',
-                './app.js',
-                './style.css',
-            ]);
+            // return cache.addAll([
+            //     './',
+            //     './index.html',
+            //     './manifest.json',
+            //     './app.js',
+            //     './style.css',
+            // ]);
+            return null;
         })
     );
 });
 
-// Our service worker will intercept all fetch requests
-// and check if we have cached the file
-// if so it will serve the cached file
 self.addEventListener('fetch', event => {
     event.respondWith(
         caches.open(cacheName)
