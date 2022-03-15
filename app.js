d9a4af9a3d6f23a06c906c2fd3c14815da1af425
Yuvraj Dagur <yuvraj.dagur928@gmail.com>
2022-03-15 20:16:10 +0530

    implement Swiper.js

--- app.js
+++ app.js
@@ -1,66 +1,44 @@
 const url = "https://www.boredapi.com/api/activity";
-var darkTheme = true;
 var activities = [];
-var currentActivity = 0;
 
 async function getActivity() {
     let response = await fetch(url);
     let activity = await response.json();
     activities.push(activity);
     document.getElementById("loader").style.display = "none";
-    document.getElementById("main").innerText = activity.activity;
-    for (let i = 0; i < 4; i++) {
+    document.getElementById("slide-0").innerText = activity.activity;
+    getNextActivities(1, 5)
+}
+
+async function getNextActivities(start, end) {
+    for (i = start; i < end; i++) {
         let response = await fetch(url);
         let activity = await response.json();
+        swiper.appendSlide(`<div class="swiper-slide"><p id="slide-${i}" class="main">${activity.activity}</p></div>`);
         activities.push(activity);
     }
+    swiper.appendSlide(`<div class="swiper-slide">
+    <button class="btn-reload" onclick="reload(${activities.length})">Gimme More!!</button>
+    <div class="spinner-border text-primary" role="status" id="loader-${activities.length}" style="display: none">
+        <span class="sr-only">Loading...</span>
+    </div>
+    </div>`);
     console.log(activities);
-    // await fetch(url).then(response => {
-    //     return response.json();
-    // }).then(data => {
-    // }).catch(e => {
-    //     document.getElementById("loader").style.display = "none";
-    //     document.getElementById("main").innerText = e;
-    // });
 };
 
-async function getNextActivity(i) {
-    if (currentActivity < activities.length) {
-        if (i === 1) {
-            currentActivity++;
-        } else {
-            if (currentActivity === 0) {
-                currentActivity = 0;
-            } else {
-                currentActivity--;
-            }
-        }
-    }
-    if (currentActivity === activities.length) {
-        await getActivity();
-    }
-    document.getElementById("main").innerText = activities[currentActivity].activity;
+async function reload(i) {
+    document.querySelector('.btn-reload').style.display = "none"
+    document.querySelector(`#loader-${i}`).style.display = "block";
+    await getNextActivities(i, i + 5);
+    swiper.slideNext();
+    swiper.removeSlide(i);
 }
 
 function changeTheme() {
-    darkTheme = !darkTheme;
     const root = document.querySelector(":root");
-    const icon = document.getElementById("icon");
-    if (darkTheme === true) {
-        icon.classList.remove("fa-sun");
-        icon.classList.add("fa-moon");
-        root.style.setProperty("--background-theme", "#040d19");
-        root.style.setProperty("--background-theme-text", "#d2e3f7");
-        root.style.setProperty("--background-theme-complementary", "#1C3144");
-        root.style.setProperty("--shadow-color", "#1c1c1c");
-    } else {
-        icon.classList.remove("fa-moon");
-        icon.classList.add("fa-sun");
-        root.style.setProperty("--background-theme", "#f2f2f2");
-        root.style.setProperty("--background-theme-text", "#060e24");
-        root.style.setProperty("--background-theme-complementary", "#d9e0ed");
-        root.style.setProperty("--shadow-color", "#949494");
-    }
+    root.style.setProperty("--background-theme", `#${Math.floor(Math.random() * 16777215).toString(16)}`);
+    root.style.setProperty("--background-theme-text", `#${Math.floor(Math.random() * 16777215).toString(16)}`);
+    root.style.setProperty("--background-theme-complementary", `#${Math.floor(Math.random() * 16777215).toString(16)}`);
 }
 
 document.addEventListener("DOMContentLoaded", getActivity);
